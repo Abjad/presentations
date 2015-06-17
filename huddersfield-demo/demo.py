9645a3ac8db850e1c93c618e011985572300f9d1
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2015-06-17 13:17:53 -0700

    Updated demo.

--- huddersfield-demo/demo.py
+++ huddersfield-demo/demo.py
@@ -6,9 +6,13 @@ from abjad import *
 
 rhythm_maker_a = rhythmmakertools.NoteRhythmMaker()
 
-divisions = [(3, 8), (5, 16), (1, 4), (3, 16)]
+divisions = [(3, 8), (5, 4), (1, 4), (13, 16)]
 
 selections = rhythm_maker_a(divisions)
+
+for selection in selections:
+    selection
+
 staff = Staff(selections, context_name='RhythmicStaff')
 show(staff)
 
@@ -32,12 +36,18 @@ sketch = make_sketch(rhythm_maker_a, divisions)
 show(sketch)
 
 divisions_b = [(5, 16), (3, 8), (3, 8), (5, 8), (1, 4)]
-sketch = make_make_sketch(rhythm_maker, divisions_b)
+sketch = make_sketch(rhythm_maker, divisions_b)
 show(sketch)
 
 divisions_b *= 20
-sketch = make_make_sketch(rhythm_maker, divisions_b)
+sketch = make_sketch(rhythm_maker, divisions_b)
 show(sketch)
+
+import random
+
+random_numerators = [random.randrange(1, 16 + 1) for x in range(100)]
+random_divisions = [(x, 32) for x in random_numerators]
+sketch = make_sketch(rhythm_maker, random_divisions)
 
 ### EXAMPLE TWO ###
 
@@ -46,61 +56,84 @@ talea = rhythmmakertools.Talea(
     counts=[1, 2, 3],
     denominator=16,
     )
-rhythm_maker_b = rhythmmakertools.TaleaRhythmMaker(talea=talea)
 
-staff = make_make_sketch(rhythm_maker_b, divisions)
+for i in range(20):
+    print(i, talea[i])
+
+### A
+
+talea_rhythm_maker = rhythmmakertools.TaleaRhythmMaker(talea=talea)
+selections = talea_rhythm_maker(divisions)
+for selection in selections:
+    selection
+sketch = make_sketch(talea_rhythm_maker, divisions)
+show(staff)
+
+### B
+
+tie_specifier = rhythmmakertools.TieSpecifier(
+    tie_across_divisions=True,
+    )
+talea_rhythm_maker = new(
+    talea_rhythm_maker,
+    tie_specifier=tie_specifier,
+    )
+staff = make_sketch(talea_rhythm_maker, divisions)
+show(staff)
+
+### C
+
+burnish_specifier = rhythmmakertools.BurnishSpecifier(
+    left_classes=(Rest, Note),
+    left_counts=(1,),
+    )
+talea_rhythm_maker = new(
+    talea_rhythm_maker,
+    burnish_specifier=burnish_specifier,
+    )
+staff = make_sketch(talea_rhythm_maker, divisions)
 show(staff)
 
 
 ### EXAMPLE THREE ###
 
 
-tie_specifier = rhythmmakertools.TieSpecifier(tie_across_divisions=True)
-rhythm_maker_c = new(rhythm_maker_b, tie_specifier=tie_specifier)
-
-staff = make_make_sketch(rhythm_maker_c, divisions)
-show(staff)
-
-
-### EXAMPLE FOUR ###
-
-
-burnish_specifier = rhythmmakertools.BurnishSpecifier(
-    left_classes=(Rest, Note),
-    left_counts=(1,),
-    )
-rhythm_maker_d = new(rhythm_maker_c, burnish_specifier=burnish_specifier)
-
-staff = make_make_sketch(rhythm_maker_d, divisions)
-show(staff)
+#def make_sketch(rhythm_maker, divisions, rotation=0):
+#    selections = rhythm_maker(divisions, rotation=rotation)
+#    measure = Measure((9, 8), selections)
+#    staff = Staff([measure], context_name='RhythmicStaff')
+#    return staff
+#
+#staff = make_sketch(rhythm_maker_d, divisions, rotation=1)
+#show(staff)
+#
+#staff = make_sketch(rhythm_maker_d, divisions, rotation=2)
+#show(staff)
+#
+#staff = make_sketch(rhythm_maker_d, divisions, rotation=3)
+#show(staff)
 
 
-### EXAMPLE FIVE ###
+### EXAMPLE THREE ###
 
 
-def make_make_sketch(rhythm_maker, divisions, rotation=0):
-    selections = rhythm_maker(divisions, rotation=rotation)
-    measure = Measure((9, 8), selections)
-    staff = Staff([measure], context_name='RhythmicStaff')
-    return staff
-
-staff = make_make_sketch(rhythm_maker_d, divisions, rotation=1)
-show(staff)
-
-staff = make_make_sketch(rhythm_maker_d, divisions, rotation=2)
-show(staff)
-
-staff = make_make_sketch(rhythm_maker_d, divisions, rotation=3)
-show(staff)
-
-
-### EXAMPLE SIX ###
-
-
-rhythmic_score = Score()
-for i in range(8):
-    staff = make_make_sketch(rhythm_maker_d, divisions, rotation=i)
-    rhythmic_score.append(staff)
+score = Score()
+for i in range(12):
+    selections = talea_rhythm_maker(divisions, rotation=i)
+    voice = Voice(selections)
+    staff = Staff([voice], context_name='RhythmicStaff')
+    score.append(staff)
     divisions = sequencetools.rotate_sequence(divisions, 1)
 
-show(rhythmic_score)
+for voice in iterate(score).by_class(Voice):
+    voice.remove_commands.append('Forbid_line_break_engraver')
+
+for staff in iterate(score).by_class(Staff):
+    override(staff).time_signature.stencil = False
+    override(staff).bar_line.stencil = False
+
+override(score).bar_number.stencil = False
+lilypond_file = lilypondfiletools.make_basic_lilypond_file(score)
+lilypond_file.layout_block.indent = 0
+
+show(lilypond_file)
